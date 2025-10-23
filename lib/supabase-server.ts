e843bef61e5019c57fcdaaaa85e40b98ecb80205
fabisiaak <patrykfabisiak1@gmail.com>
2025-10-23 17:44:40 +0200

    Task #11 - setup supabase SDK

---
+++ lib/supabase-server.ts
@@ -0,0 +1,25 @@
+import "server-only";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+  const store = await cookies(); // <— tu był problem
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return store.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          store.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          store.set({ name, value: "", ...options });
+        },
+      },
+    }
+  );
+}
